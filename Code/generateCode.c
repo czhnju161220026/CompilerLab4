8ba312e6f61d636d9fc75076494a7f2a6cb45f2c
LintianShi <254656131@qq.com>
2019-12-10 03:24:38 -0800

    parser line

--- Code/generateCode.c
+++ Code/generateCode.c
@@ -24,3 +24,29 @@ char *handleLine(Line *line)
     //TODO: implement
     return result;
 }
+
+Notation* parserLine(char* content) {
+    Notation* head = NULL;
+    Notation* p = NULL;
+    char* temp = strtok(content, " ");
+    while (temp != NULL) {
+        Notation* t = createNotation(temp);
+        if (head == NULL && p == NULL) {
+            head = t;
+            p = t;
+        } else {
+            p->next = t;
+            p = t;
+        }
+        temp = strtok(NULL, " ");
+    }
+    return head;
+}
+
+Notation* createNotation(char* content) {
+    Notation* notation = (Notation*) malloc(sizeof(Notation));
+    notation->next = NULL;
+    notation->content = (char*) malloc(strlen(content) + 2);
+    strcpy(notation->content, content);
+    return notation;
+}
--- Code/generateCode.h
+++ Code/generateCode.h
@@ -4,8 +4,15 @@
 #include "utils.h"
 #include "addrDescriptor.h"
 
+typedef struct Notation {
+    char* content;
+    struct Notation* next;
+} Notation;
+
 char* generateCode(Function* functions);
 char* handleFunction(Function* function);
-char* handleLine(Line* line);
+char* handleLine(Line* line); 
+Notation* parserLine(char* content);
+Notation* createNotation(char* content);
 
 #endif
